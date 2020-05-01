c6ce88b090456b24b23503f62b81019f268cca7d
Mikey Roush <mikey.roush97@gmail.com>
2020-05-01 13:09:13 -0500

    implemented functionality for incorrect guesses

--- hangman.py
+++ hangman.py
@@ -23,16 +23,19 @@ class Man(ShapeNode):
 		ShapeNode.__init__(self,**kwargs)
 		#possibly make body part sizes functions of man_space...
 		self.width = 7
+		self.start_color = 'white'
+		self.start_alpha = 0.1
+		self.end_alpha = 1
 		#build head
 		head_shape = ui.Path.oval(0,0,100,100)
 		head_shape.line_width = self.width
 		headY = self.size.h/2
-		head = ShapeNode(head_shape,fill_color='clear',stroke_color='white',parent=self,position=(0,headY))
+		head = ShapeNode(head_shape,fill_color='clear',stroke_color=self.start_color,parent=self,position=(0,headY),alpha=self.start_alpha)
 		head.anchor_point = (0.5,1)
 		#build body
 		body_shape = ui.Path.rect(0,0,self.width,200)
 		bodyY = headY - 100
-		body = ShapeNode(body_shape,fill_color='white',stroke_color='clear',parent=self,position=(0,bodyY))
+		body = ShapeNode(body_shape,fill_color=self.start_color,parent=self,position=(0,bodyY),alpha=self.start_alpha)
 		body.anchor_point = (0.5,1)
 		#build arms
 		armY = bodyY - 50
@@ -42,7 +45,9 @@ class Man(ShapeNode):
 		legY = headY - 295
 		left_leg = self.make_limb(0,legY,-50,150)
 		right_leg = self.make_limb(0,legY,50,150)
-		#setup lives system...
+		#guesses system
+		self.parts = [head, body, left_arm, right_arm, left_leg, right_leg]
+		self.incorrect_guesses = 0
 		
 	def make_limb(self,x,y,changeX,changeY):
 		#set anchor point in the top left or top right corner
@@ -56,9 +61,14 @@ class Man(ShapeNode):
 		limb_shape.line_to(changeX,changeY)
 		limb_shape.close()
 		#make shape node and add anchor point
-		limb = ShapeNode(limb_shape,stroke_color='white',parent=self,position=(x,y))
+		limb = ShapeNode(limb_shape,stroke_color=self.start_color,parent=self,position=(x,y),alpha=self.start_alpha)
 		limb.anchor_point = (anchorX,1)
 		return limb
+		
+	def add_part(self):
+		if self.incorrect_guesses in range(len(self.parts)):
+			self.parts[self.incorrect_guesses].alpha = self.end_alpha
+			self.incorrect_guesses+=1
 
 class Hangman(Scene):
 	
@@ -99,23 +109,31 @@ class Hangman(Scene):
 	def touch_began(self, touch):
 		#listen for touch events on letters
 		#if any of the letters are touched, replace them with a dot
-		for let in self.letters:
-			if let.is_touched(touch.location.x,touch.location.y):
-				self.guess_letter(let.text)
-				self.letters.remove(let)
-				let.run_action(Action.remove())
-				temp = SpriteNode('plf:LaserPurpleDot',position=let.position,parent=self)
+		if self.man.incorrect_guesses < 6:
+			for let in self.letters:
+				if let.is_touched(touch.location.x,touch.location.y):
+					self.guess_letter(let.text)
+					self.letters.remove(let)
+					let.run_action(Action.remove())
+					temp = SpriteNode('plf:LaserPurpleDot',position=let.position,parent=self)
 				
 	def replace_character(self,string,new_string,index):
+		#thow error if the index is not within the bounds of the string
 		if index not in range(len(string)):
 			raise ValueError("index outside given string")
 		else:
 			return string[:index] + new_string + string[index+1:]
 				
 	def guess_letter(self,let):
-		index = self.word.find(let)
-		if index > -1:
-			blanks = self.replace_character(self.answer.text,let,2*index)
-			self.answer.text = blanks
+		#replace blank with letter if it is right
+		#else take away a life
+		found = False
+		for index, right_letter in enumerate(self.word):
+			if let == right_letter:
+				found = True
+				blanks = self.replace_character(self.answer.text,let,2*index)
+				self.answer.text = blanks
+		if not found:
+			self.man.add_part()		
 			
 run(Hangman())
